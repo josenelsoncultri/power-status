88e03a2f63ba8674f29d4edf35d40933b7bc58a7
josenelsoncultri <josefcultrifrc@gmail.com>
2018-10-07 21:56:07 -0300

    Acréscimo da imagem da bateria e renomeação do formulário principal

---
+++ frmPowerStatus.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace PowerStatus
+{
+    partial class frmPowerStatus
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.components = new System.ComponentModel.Container();
+			this.tmrPower = new System.Windows.Forms.Timer(this.components);
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// tmrPower
+			// 
+			this.tmrPower.Enabled = true;
+			this.tmrPower.Interval = 1000;
+			this.tmrPower.Tick += new System.EventHandler(this.tmrPower_Tick);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::PowerStatus.Properties.Resources.PowerBattery;
+			this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(281, 140);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// frmPowerStatus
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(305, 164);
+			this.Controls.Add(this.pictureBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.KeyPreview = true;
+			this.Name = "frmPowerStatus";
+			this.Text = "Form1";
+			this.TransparencyKey = System.Drawing.SystemColors.Control;
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tmrPower;
+		private System.Windows.Forms.PictureBox pictureBox1;
+	}
+}
+
---
+++ frmPowerStatus.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PowerStatus
+{
+    public partial class frmPowerStatus : Form
+    {
+        Point mouseDownPoint = Point.Empty;
+
+        public frmPowerStatus()
+        {
+            InitializeComponent();
+        }
+
+        private void tmrPower_Tick(object sender, EventArgs e)
+        {
+            string powerPercent = Math.Round(SystemInformation.PowerStatus.BatteryLifePercent * 100, MidpointRounding.AwayFromZero).ToString() + "%";
+        }
+    }
+}
